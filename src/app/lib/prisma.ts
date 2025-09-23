b117aadcb6c9d89e729d7f29d362e03dfe6d6f1a
Wade Foster The Black Machine <172018441+wade-levels-up@users.noreply.github.com>
2025-09-23 16:40:22 +1000

    Add logging to prisma.ts to debug connection issue

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -4,6 +4,13 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL
+      }
+    },
+    log: ['query', 'info', 'warn', 'error'],
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
