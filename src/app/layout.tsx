d26ed68d9bd54d70ba4941501458cf833b219d1d
Wade Foster The Black Machine <172018441+wade-levels-up@users.noreply.github.com>
2025-09-24 16:50:25 +1000

    fix: Remove overflow from main section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
               </SignedIn>
             </div>
           </header>
-          <div className="grow overflow-auto">{children}</div>
+          <div className="grow">{children}</div>
           <footer className="md:flex justify-end items-center p-4 gap-4 h-16 border-t-1 hidden">
             <a href="https://wadelevelsup.com/" target="blank">
               Made by Wade
